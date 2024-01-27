4ceb4fab766a0a6c6e3962c760e428618d459ca6
BobdaProgrammer <144843651+BobdaProgrammer@users.noreply.github.com>
2024-01-27 11:38:09 +0000

    NotWorking

---
+++ index.js
@@ -0,0 +1,44 @@
+function getIt () {
+  const apiKey = "ZcLKnh1pUAzH3DNO64glYWZ9494pa3P6aN3VBdd2";
+  const apiUrl = 'https://api.printful.com/orders';
+
+async function fetchProducts() {
+  fetch(`${apiUrl}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Basic ${btoa(apiKey)}`,
+      Connection: `keep-alive`,
+      Accept: `*/*`,
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json(); // Parse the response into JSON
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching products:", error);
+    });
+}
+
+
+  function displayProducts(products) {
+    const productsList = document.getElementById("products-list");
+
+    products.forEach(function (product) {
+      const productElement = document.createElement("div");
+      productElement.innerHTML = `
+                <h2>${product.name}</h2>
+                <img src="${product.sync_product.thumbnail_url}" alt="${product.name}">
+                <p>Price: $${product.sync_product.retail_price}</p>
+            `;
+      productsList.appendChild(productElement);
+    });
+  }
+
+  fetchProducts();
+}
